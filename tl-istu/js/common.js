fc33aa82f1cd3baf711d1f454f768cb12f4fafb5
MaximSurovtsev <gr4m0zeka@gmail.com>
2018-01-27 20:32:44 +0300

    initial

---
+++ tl-istu/js/common.js
@@ -0,0 +1,40 @@
+$(function() {
+
+	$('.form, .handheld-form').on('click', function() {
+
+		if ($('.form-container').hasClass('appear')) {
+			$('.form-container').toggleClass('appear');
+			setTimeout(function() { $('.form-container').toggleClass('isDisplay'); }, 510);
+		} else {
+			$('.form-container').toggleClass('isDisplay');
+			setTimeout(function() { $('.form-container').toggleClass('appear'); }, 10)
+		}
+		
+	});
+
+	$('.toggle-menu').on('click', function() {
+		$('.toggle-menu').toggleClass('active');
+
+		if ($('.handheld-nav-ul').hasClass('appear')) {
+			$('.handheld-nav-ul').toggleClass('appear');
+			setTimeout(function() { $('.handheld-nav-ul').toggleClass('isDisplay'); }, 510);
+		} else {
+			$('.handheld-nav-ul').toggleClass('isDisplay');
+			setTimeout(function() { $('.handheld-nav-ul').toggleClass('appear'); }, 10)
+		}
+	});
+	var wWidth = $(window).width();
+
+	function heightDetect() {
+		$("body").css("height", $(window).height());
+	};
+
+	heightDetect();
+			
+	if ( wWidth > 480 ) {
+		$(window).resize(function() {
+			heightDetect();
+		});
+	}
+
+});
