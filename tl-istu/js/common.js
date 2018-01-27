cacf36d043c5782fdcf0b012ed76e8a3dfa9aaa3
MaximSurovtsev <gr4m0zeka@gmail.com>
2018-01-27 22:07:24 +0300

    fix

--- tl-istu/js/common.js
+++ tl-istu/js/common.js
@@ -1,11 +1,23 @@
 $(function() {
+	
 
+	var freezeVp = function(e) {
+    e.preventDefault();
+	};
+	function stopBodyScrolling (bool) {
+    if (bool === true) {
+        document.body.addEventListener("touchmove", freezeVp, false);
+    } else {
+        document.body.removeEventListener("touchmove", freezeVp, false);
+    }
+	}
 	$('.form, .handheld-form').on('click', function() {
 
 		if ($('.form-container').hasClass('appear')) {
 			$('.form-container').toggleClass('appear');
 			$('body').toggleClass('disable-scroll');
 			$('html').css('overflow', 'auto');
+			stopBodyScrolling(false);
 			setTimeout(function() { 
 				$('.form-container').toggleClass('isDisplay'); 
 				
@@ -13,6 +25,7 @@ $(function() {
 		} else {
 			$('body').toggleClass('disable-scroll');
 			$('html').css('overflow', 'hidden');
+			stopBodyScrolling(true);
 			$('.form-container').toggleClass('isDisplay');
 			setTimeout(function() { $('.form-container').toggleClass('appear'); }, 10)
 		}
@@ -26,6 +39,7 @@ $(function() {
 			$('.handheld-nav-ul').toggleClass('appear');
 			$('body').toggleClass('disable-scroll');
 			$('html').css('overflow', 'auto');
+			stopBodyScrolling(false);
 			setTimeout(function() { 
 				$('.handheld-nav-ul').toggleClass('isDisplay'); 
 				
@@ -33,6 +47,7 @@ $(function() {
 		} else {
 			$('body').toggleClass('disable-scroll');
 			$('html').css('overflow', 'hidden');
+			stopBodyScrolling(true);
 			$('.handheld-nav-ul').toggleClass('isDisplay');
 			setTimeout(function() { $('.handheld-nav-ul').toggleClass('appear'); }, 10)
 		}
